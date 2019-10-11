b35b1ab038100e4684659fd8a9dc8f1163d2e352
Evertras <bfullj@gmail.com>
2019-10-11 17:06:12 +0900

    Add install/delete tasks

--- auth/cmd/server/main.go
+++ auth/cmd/server/main.go
@@ -21,7 +21,7 @@ func checkAuthHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("OK!", authToken)
 
 	// Right now everyone can get by as long as they have any token, super secure!
-	w.Header().Set("X-User-ID", authToken)
+	w.Header().Set("X-User-ID", "user-" + authToken)
 
 	w.WriteHeader(200)
 }
